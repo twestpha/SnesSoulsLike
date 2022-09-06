b59332507525029b6ced05c9105a01a9e7efdfcd
twestpha <trevorawestphal@gmail.com>
2022-09-05 20:10:56 -0600

    Making camera lerp between units rather than cut

--- Assets/Scripts/Components/Player And Camera Components/PlayerComponent.cs
+++ Assets/Scripts/Components/Player And Camera Components/PlayerComponent.cs
@@ -5,57 +5,78 @@ using UnityEngine.UI;
 
 class PlayerComponent : MonoBehaviour {
 
+    private const float CAMERA_MOVE_TIME = 0.75f;
+
     public static PlayerComponent player;
 
     public UnitComponent[] units;
-    public Camera[] cameras;
+    public Transform[] cameraTargetTransforms;
+
+    public Transform cameraTransform;
 
     private int index;
 
+    private Timer cameraMoveTimer = new Timer(CAMERA_MOVE_TIME);
+
+    private Vector3 previousCameraPosition;
+    private Vector3 previousCameraForward;
+
     void Start(){
         player = this;
-
-        // For now
-        cameras[index].gameObject.SetActive(true);
     }
 
     void Update(){
         if(Input.GetKeyDown(KeyCode.E)){
             units[index].SetInputDirection(Vector3.zero);
             units[index].GetComponent<PlayerUnitAIComponent>().enabled = true;
-            cameras[index].gameObject.SetActive(false);
 
             index = (index + units.Length + 1) % units.Length;
 
-            cameras[index].gameObject.SetActive(true);
             units[index].GetComponent<PlayerUnitAIComponent>().enabled = false;
+
+            cameraMoveTimer.Start();
+
+            previousCameraPosition = cameraTransform.position;
+            previousCameraForward = cameraTransform.forward;
         }
 
         if(Input.GetKeyDown(KeyCode.Q)){
             units[index].SetInputDirection(Vector3.zero);
             units[index].GetComponent<PlayerUnitAIComponent>().enabled = true;
-            cameras[index].gameObject.SetActive(false);
 
             index = (index + units.Length - 1) % units.Length;
 
-            cameras[index].gameObject.SetActive(true);
             units[index].GetComponent<PlayerUnitAIComponent>().enabled = false;
+
+            cameraMoveTimer.Start();
+
+            previousCameraPosition = cameraTransform.position;
+            previousCameraForward = cameraTransform.forward;
         }
+
+        // Camera movement
+        float cameraT = CustomMath.EaseInOut(cameraMoveTimer.Parameterized());
+
+        Vector3 cameraTargetPosition = cameraTargetTransforms[index].position;
+        Vector3 cameraTargetOrientation = cameraTargetTransforms[index].forward;
+
+        cameraTransform.position = Vector3.Lerp(previousCameraPosition, cameraTargetPosition, cameraT);
+        cameraTransform.rotation = Quaternion.LookRotation(Vector3.Lerp(previousCameraForward, cameraTargetOrientation, cameraT));
 
         // Player Input
         Vector3 inputDirection = Vector3.zero;
 
         if(Input.GetKey(KeyCode.W)){
-            inputDirection += cameras[index].transform.forward;
+            inputDirection += cameraTargetTransforms[index].transform.forward;
         }
         if(Input.GetKey(KeyCode.A)){
-            inputDirection -= cameras[index].transform.right;
+            inputDirection -= cameraTargetTransforms[index].transform.right;
         }
         if(Input.GetKey(KeyCode.S)){
-            inputDirection -= cameras[index].transform.forward;
+            inputDirection -= cameraTargetTransforms[index].transform.forward;
         }
         if(Input.GetKey(KeyCode.D)){
-            inputDirection += cameras[index].transform.right;
+            inputDirection += cameraTargetTransforms[index].transform.right;
         }
 
         units[index].SetInputDirection(inputDirection);
