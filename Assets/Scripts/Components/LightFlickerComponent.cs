7c8fca384d7684d73848b525da15463359f9f90b
twestpha <trevorawestphal@gmail.com>
2022-09-11 11:46:32 -0600

    testing out lightable braziers

--- Assets/Scripts/Components/LightFlickerComponent.cs
+++ Assets/Scripts/Components/LightFlickerComponent.cs
@@ -11,12 +11,16 @@ class LightFlickerComponent : MonoBehaviour {
     private float maxIntensity;
     private Light flickerLight;
 
+    private float offset;
+
     void Start(){
+        offset = UnityEngine.Random.value;
+
         flickerLight = GetComponent<Light>();
         maxIntensity = flickerLight.intensity;
     }
 
     void Update(){
-        flickerLight.intensity = brightness.Evaluate((Time.time * flickerSpeed) % 1.0f) * maxIntensity;
+        flickerLight.intensity = brightness.Evaluate(((Time.time * offset) * flickerSpeed) % 1.0f) * maxIntensity;
     }
 }
